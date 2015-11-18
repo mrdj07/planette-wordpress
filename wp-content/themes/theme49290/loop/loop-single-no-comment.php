2b937357c279984bc5b36b0bb03d6af49aab4f96
Mathieu Savage <savage.mathieu@gmail.com>
2015-11-17 21:44:22 -0500

    Enlevé comments dans promo pages

---
+++ wp-content/themes/theme49290/loop/loop-single-no-comment.php
@@ -0,0 +1,14 @@
+<?php /* Loop Name: Single */ ?>
+<?php if (have_posts()) : while (have_posts()) : the_post();
+	// The following determines what the post format is and shows the correct file accordingly
+	$format = get_post_format();
+	get_template_part( 'includes/post-formats/'.$format );
+	if($format == '')
+		get_template_part( 'includes/post-formats/standard-no-meta' );
+	get_template_part( 'includes/post-formats/share-buttons' );
+	wp_link_pages('before=<div class="pagination">&after=</div>');
+?>
+
+<?php
+	endwhile; endif;
+?>
